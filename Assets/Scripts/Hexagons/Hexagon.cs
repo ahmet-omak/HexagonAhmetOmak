ce35ef2f8255331ba9196b339647fa1acddbbee8
Ahmet Omak <ahmetaltioglu@hotmail.com>
2021-02-15 22:47:47 +0300

    Switch hexagons logic added

--- Assets/Scripts/Hexagons/Hexagon.cs
+++ Assets/Scripts/Hexagons/Hexagon.cs
@@ -1,4 +1,5 @@
 ﻿using Assest.Scripts.General;
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -33,6 +34,7 @@ namespace Assest.Scripts.Hexagons
 
         private void PickNeighbours()
         {
+            List<Hexagon> selectedHexagons;
             int x1, y1, x2, y2;
             for (int i = 0; i < xFactorsOf.Length - 1; i++)
             {
@@ -42,12 +44,27 @@ namespace Assest.Scripts.Hexagons
                 y2 = yFactorsOf[i + 1];
                 if ((x1 + x >= 0 && x1 + x <= 7) && (y1 + y >= 0 && y1 + y <= 8) && (x2 + x >= 0 && x2 + x <= 7) && (y2 + y >= 0 && y2 + y <= 8))
                 {
-                    GridManager.grid[x, y].GetComponent<SpriteRenderer>().color = Color.white;
-                    GridManager.grid[x1 + x, y1 + y].GetComponent<SpriteRenderer>().color = Color.white;
-                    GridManager.grid[x2 + x, y2 + y].GetComponent<SpriteRenderer>().color = Color.white;
+                    selectedHexagons = new List<Hexagon>();
+                    selectedHexagons.Add(GridManager.grid[x1 + x, y1 + y].GetComponent<Hexagon>());
+                    selectedHexagons.Add(GridManager.grid[x2 + x, y2 + y].GetComponent<Hexagon>());
+                    Debug.Log(selectedHexagons[0].name);
+                    Debug.Log(selectedHexagons[1].name);
+                    SwitchHexagons(selectedHexagons);
                     return;
                 }
             }
+        }
+
+        private void SwitchHexagons(List<Hexagon> selectedHexagons)
+        {
+            Vector3 temporaryHexagonPosition = selectedHexagons[0].gameObject.transform.position;
+            selectedHexagons[0].gameObject.transform.position = selectedHexagons[1].gameObject.transform.position;
+            selectedHexagons[1].gameObject.transform.position = temporaryHexagonPosition;
+        }
+
+        private void Check()
+        {
+            //TODO: Implement this function
         }
     }
 }
